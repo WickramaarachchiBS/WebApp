3b3e8ba34b5f23aeb3a694acf4d5d66222dd654c
WickramaarachchiBS <wickramaarachchibs@students.nsbm.ac.lk>
2024-12-02 19:43:42 +0530

    added Movie>DTO class

---
+++ demo/src/main/java/org/example/demo/dto/Movie.java
@@ -0,0 +1,49 @@
+package org.example.demo.dto;
+
+public class Movie {
+    int id;
+    String title;
+    String desc;
+    String price;
+    String imagePath;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+}
